79a5ba380697ca9462020ce149c86883d5b7815d
AlumnoPracticas2 <alumno.practicas2@avantelite.es>
2026-05-05 12:51:45 +0200

    style: alinear propiedades en config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ var AVANTSERVICE_CONFIG = (function () {
     var isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
     var BASE_URL = isLocal ? 'http://localhost:3000' : 'https://api-webs.avantelite.es';
     return {
-        BASE_URL:    BASE_URL,
-        API_BLOG:    BASE_URL + '/api/blog',
+        BASE_URL: BASE_URL,
+        API_BLOG: BASE_URL + '/api/blog',
         API_CONTACT: BASE_URL + '/contacto',
-        API_TRACK:   BASE_URL + '/api/track',
-        API_BUDGET:   BASE_URL + '/api/presupuesto',
+        API_TRACK: BASE_URL + '/api/track',
+        API_BUDGET: BASE_URL + '/api/presupuesto',
         API_SERVICIOS: BASE_URL + '/api/servicios',
     };
 })();
